c33f29c46cd5eb729c3120aa90a7a832d7195e8b
Korjubzot <korjubzot@gmail.com>
2024-04-02 12:41:05 +0200

    Restyled layout of albumData page

--- client/src/components/albumData/albumData.js
+++ client/src/components/albumData/albumData.js
@@ -29,37 +29,38 @@ function AlbumData() {
     return <div className="text-blue-500 font-bold text-xl">Loading...</div>;
   } else {
     return (
-      <div
-        data-testid="album-data"
-        className="flex flex-col justify-center items-center bg-gray-100 max-h-screen"
-      >
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          {albumsData &&
-            albumsData.map((album, index) => {
-              return (
-                <div
-                  key={index}
-                  className="my-2 bg-white p-6 rounded-lg shadow-md"
-                >
-                  <h1 className="text-green-600 text-4xl mb-2">
-                    {album.title}
-                  </h1>
-                  <h2 className="text-gray-700 text-2xl mb-2">
-                    by {album.artist}
-                  </h2>
-                  <p className="text-gray-500">
-                    Released in {album.release_year}
-                  </p>
-                </div>
-              );
-            })}
+      <div className="flex justify-center items-center bg-gray-100 max-h-screen">
+        <div className="flex flex-col mr-4">
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            {albumsData &&
+              albumsData.map((album, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="my-2 bg-white p-6 rounded-lg shadow-md"
+                  >
+                    <h1 className="text-green-600 text-4xl mb-2">
+                      {album.title}
+                    </h1>
+                    <h2 className="text-gray-700 text-2xl mb-2">
+                      by {album.artist}
+                    </h2>
+                    <p className="text-gray-500">
+                      Released in {album.release_year}
+                    </p>
+                  </div>
+                );
+              })}
+          </div>
+          <Player />
         </div>
-        <Player />
-        <img
-          className="max-w-lg max-h-lg mt-4 rounded-lg shadow-md"
-          src={img}
-          alt="album art"
-        />
+        <div>
+          <img
+            className="max-w-lg max-h-lg mt-4 rounded-lg shadow-md"
+            src={img}
+            alt="album art"
+          />
+        </div>
       </div>
     );
   }
