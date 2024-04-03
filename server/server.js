9912eeade7a9fd1c7aa1488170cc9068386f2a1f
Korjubzot <korjubzot@gmail.com>
2024-04-03 15:03:38 +0200

    Added basic routing for albums

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const pool = new pg.Pool({
 });
 
 // TODO MOVE TO .ENV
+// TODO seperate routes and handlers
 
 app.get("/ping", async (req, res) => {
   res.status(200).json({ message: "Hello World" });
@@ -50,21 +51,69 @@ app.get("/albums", async (req, res) => {
   }
 });
 
-app.post(
-  ("/albums",
-  async (req, res) => {
-    const { title, artist, release_year, genre } = req.body;
-    try {
-      const { rows } = await pool.query(
-        "INSERT INTO albums (title, artist, release_year, genre) VALUES ($1, $2, $3, $4) RETURNING *",
-        [title, artist, release_year, genre]
-      );
+app.get("/albums/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM albums WHERE album_id = $1",
+      [id]
+    );
+    if (rows.length > 0) {
       res.json(rows[0]);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+    } else {
+      res.status(404).json({ error: "Album not found" });
     }
-  })
-);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/albums", async (req, res) => {
+  const { title, artist, release_year, genre } = req.body;
+  try {
+    const { rows } = await pool.query(
+      "INSERT INTO albums (title, artist, release_year, genre) VALUES ($1, $2, $3, $4) RETURNING *",
+      [title, artist, release_year, genre]
+    );
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/albums/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, artist, release_year, genre } = req.body;
+  try {
+    const { rows } = await pool.query(
+      "UPDATE albums SET title = $1, artist = $2, release_year = $3, genre = $4 WHERE id = $5 RETURNING *",
+      [title, artist, release_year, genre, id]
+    );
+    if (rows.length > 0) {
+      res.json(rows[0]);
+    } else {
+      res.status(404).json({ error: "Album not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/albums/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { rowCount } = await pool.query("DELETE FROM albums WHERE id = $1", [
+      id,
+    ]);
+    if (rowCount > 0) {
+      res.json({ message: "Album deleted" });
+    } else {
+      res.status(404).json({ error: "Album not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
