f80e2c47841549bec95930cccf9106ce41e8275c
Korjubzot <korjubzot@gmail.com>
2024-04-03 16:04:48 +0200

    Added .env file to server

--- server/db.js
+++ server/db.js
@@ -1,14 +1,12 @@
-const pg = require("pg");
+require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new pg.Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "password",
-  port: 5432,
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
-
-// TODO MOVE TO .ENV
 
 module.exports = pool;
