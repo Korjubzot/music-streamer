9a0d964258b95ac540f3425dc90f679f30731e50
Korjubzot <korjubzot@gmail.com>
2024-08-15 12:00:09 +0200

    Updated tests for albumPlayer

--- client/src/components/albumPlayer/albumPlayer.js
+++ client/src/components/albumPlayer/albumPlayer.js
@@ -28,19 +28,21 @@ function AlbumPlayer() {
   } else {
     return (
       <>
-        <div className="flex flex-col mr-4 justify-center items center bg-gray-100">
-          <h1 className="text-gray-600 text-4xl mb-2">{albumsData.title}</h1>
-          <h2 className="text-gray-500 text-2xl mb-2">
+        <div className="flex flex-col px-4 justify-center items center bg-gray-100">
+          <h1 className="text-gray-600 text-4xl mt-2 ">{albumsData.title}</h1>
+          <h2 className="text-gray-500 text-2xl mb-2 ">
             by {albumsData.artist}
           </h2>
-          <p className="text-gray-500">Released in {albumsData.release_year}</p>
+          <p className="text-gray-500 ">
+            Released in {albumsData.release_year}
+          </p>
           <img
-            className="max-w-lg max-h-lg mt-4 rounded-lg shadow-md"
+            className="max-w-lg max-h-lg my-4 rounded-lg shadow-md"
             src={img}
             alt="album art"
           />
+          <Player />
         </div>
-        <Player />
       </>
     );
   }
